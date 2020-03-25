0f4fa1d2b388c4cffbf1f605ab10fd3ff4b287b0
m19e <nexus.me7290@gmail.com>
2020-03-25 21:55:02 +0900

    円を描くまで

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"image"
 	"image/color"
 	"image/jpeg"
+	"math"
 	"os"
 )
 
@@ -33,15 +34,39 @@ func drawBounds(img *image.RGBA, col color.Color) {
 	}
 }
 
+type Circle struct {
+	p image.Point
+	r int
+}
+
+func (c *Circle) drawCircle(img *image.RGBA, col color.Color) {
+	for rad := 0.0; rad < 2.0*float64(c.r); rad += 0.1 {
+		x := int(float64(c.p.X) + float64(c.r)*math.Cos(rad))
+		y := int(float64(c.p.Y) + float64(c.r)*math.Sin(rad))
+		img.Set(x, y, col)
+	}
+}
+
 func main() {
 	x := 0
 	y := 0
-	width := 100
-	height := 50
+	width := 500
+	height := 500
 
 	img := image.NewRGBA(image.Rect(x, y, width, height))
+	// dye White
 	fillRect(img, color.RGBA{255, 255, 255, 0})
-	drawBounds(img, color.RGBA{255, 0, 0, 0})
+
+	// draw Bounds
+	// drawBounds(img, color.RGBA{255, 0, 0, 0})
+
+	// draw Circle
+	// center point
+	center := image.Point{250, 250}
+	// create circle
+	circle := Circle{center, 50}
+	// Draw
+	circle.drawCircle(img, color.RGBA{255, 0, 0, 0})
 
 	file, _ := os.Create("sample.jpg")
 	defer file.Close()
